986134bd75f5e00fce38a85a42a517ccba6c5532
ropoko <rodrigostramantinoli@gmail.com>
2022-09-02 23:29:41 -0300

    initial commit (๑ↀᆺↀ๑)

---
+++ GameInfantil/Forms/Main.cs
@@ -0,0 +1,15 @@
+namespace GameInfantil
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
