d086e6b8610e5bad89413886f828bac6a13be887
ropoko <rodrigostramantinoli@gmail.com>
2022-09-03 01:02:11 -0300

    feat: added models for questions and answers

---
+++ GameInfantil/Models/QuizAnswer.cs
@@ -0,0 +1,12 @@
+﻿namespace GameInfantil.Models
+{
+    /// <summary>
+    /// Define um tipo padrão para as respostas - contendo o texto 
+    /// que será exibido e se é a resposta correta ou não
+    /// </summary>
+    public class QuizAnswer
+    {
+        public string? Text { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+}
---
+++ GameInfantil/Models/QuizQuestion.cs
@@ -0,0 +1,14 @@
+﻿namespace GameInfantil.Models
+{
+    public class QuizQuestion
+    {
+        public string? Question { get; set; }
+        public QuizAnswer? Option1 { get; set; }
+        public QuizAnswer? Option2 { get; set; }
+        public QuizAnswer? Option3 { get; set; }
+        public QuizAnswer? Option4 { get; set; }
+
+        // indica se a pergunta já foi respondida ou não
+        public bool Answered { get; set; }
+    }
+}
